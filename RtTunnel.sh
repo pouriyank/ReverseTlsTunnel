e3b9505063fcacdc0bd9aa07ce26d48f5da4f4bc
pouriyank <30825749+pouriyank@users.noreply.github.com>
2023-10-14 10:31:05 +0330

    Update RtTunnel.sh

--- RtTunnel.sh
+++ RtTunnel.sh
@@ -7,7 +7,33 @@ check_dependencies() {
         sudo apt-get install wget
     fi
 }
+install_droop() {
+clear
+dropport=222
+echo -e "\nPlease input DropBear Port."
+printf "Default Port is \e[33m${dropport}\e[0m, let it blank to use this Port: "
+read dropporttmp
+if [[ -n "${dropporttmp}" ]]; then
+    dropport=${dropporttmp}
+fi
 
+sudo apt update -y
+sudo apt install dropbear -y
+cat >  /etc/default/dropbear << ENDOFFILE
+NO_START=0
+DROPBEAR_PORT=$dropport
+DROPBEAR_EXTRA_ARGS=""
+DROPBEAR_RSAKEY="/etc/dropbear/dropbear_rsa_host_key"
+DROPBEAR_DSSKEY="/etc/dropbear/dropbear_dss_host_key"
+DROPBEAR_ECDSAKEY="/etc/dropbear/dropbear_ecdsa_host_key"
+DROPBEAR_RECEIVE_WINDOW=65536
+ENDOFFILE
+sudo ufw allow $dropport
+sudo systemctl restart dropbear
+sudo systemctl enable dropbear
+clear
+echo "DROPBEAR Installed As Port : $dropport"
+}
 #Check installed service
 check_installed() {
     if [ -f "/etc/systemd/system/tunnel.service" ]; then
@@ -133,6 +159,7 @@ echo " --------#- Reverse Tls Tunnel -#--------"
 echo "1) Install"
 echo "2) Uninstall"
 echo "3) Check Update"
+echo "4) Install drop "
 echo "0) Exit"
 echo " --------------$version---------------"
 read -p "Please choose: " choice
@@ -145,6 +172,9 @@ case $choice in
         uninstall
         ;;
     3) check_update
+        ;;
+    4)
+        install drop
         ;;
     0)
         exit
